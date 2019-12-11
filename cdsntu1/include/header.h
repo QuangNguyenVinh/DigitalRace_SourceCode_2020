afcfcdcf601b8e84276e4011e49f9f2cb42c7c4c
quang <vinhquangnguyen2@gmail.com>
2019-12-11 21:07:22 +0700

    add #include <opencv2/ml/ml.hpp>

--- cdsntu1/include/header.h
+++ cdsntu1/include/header.h
@@ -6,6 +6,7 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/core/core.hpp>
+#include <opencv2/ml/ml.hpp>
 
 #include <ros/ros.h>
 #include "std_msgs/Float32.h"
